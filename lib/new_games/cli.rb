8ef1de2543b2d43ce6e51f9bf7ce36b37028a5ae
Lucas Kisabeth <lukey.mac@outlook.com>
2017-04-10 23:58:11 +0000

    extended functionality of #start method

--- lib/new_games/cli.rb
+++ lib/new_games/cli.rb
@@ -1,7 +1,7 @@
 class NewGames::CLI
 
   def call
-    start
+    self.start
   end
 
   def list_platforms
@@ -18,7 +18,25 @@ class NewGames::CLI
   end
 
   def start
-    list_platforms
+    self.list_platforms
+    input = nil
+    while input != "exit"
+      puts ""
+      puts "Enter the corresponding number to select a platform:"
+      input = gets.strip
+      if input.to_i == 0 && input != "exit"
+        puts ""
+        puts "Please enter the number, not the platform name!"
+        puts "Restarting..."
+        sleep(2)
+        start
+      elsif input.to_i > 0
+        puts "LIST OF [PLATFORM] GAMES HERE"
+      end
+    end
+    puts ""
+    puts "Hope you found something you like!"
+    puts "Have a great day! :D"
   end
 
 end
