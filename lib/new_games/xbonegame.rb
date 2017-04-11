8f50faf557b6cec4d50d2741b585b211bdf80be3
Lucas Kisabeth <lukey.mac@outlook.com>
2017-04-11 00:59:50 +0000

    added #initialize and #self.all methods to NewGames::XboneGame class

--- lib/new_games/xbonegame.rb
+++ lib/new_games/xbonegame.rb
@@ -1,4 +1,13 @@
 class NewGames::XboneGame
   attr_accessor :name, :url, :release_date, :metascore, :publisher, :also_on
 
+  def initialize(name = nil, url = nil)
+    @name = name
+    @url = url
+  end
+
+  def self.all
+    @@all ||= scrape_xbone_games
+  end
+  
 end
