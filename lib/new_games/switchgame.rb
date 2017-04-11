59e8afa3f1c6c3c07f387a43129e6907b1acd3d4
Lucas Kisabeth <lukey.mac@outlook.com>
2017-04-11 00:58:40 +0000

    added #initialize and #self.all methods to NewGames::SwitchGame class

--- lib/new_games/switchgame.rb
+++ lib/new_games/switchgame.rb
@@ -1,4 +1,13 @@
 class NewGames::SwitchGame
   attr_accessor :name, :url, :release_date, :metascore, :publisher, :also_on
 
+  def initialize(name = nil, url = nil)
+    @name = name
+    @url = url
+  end
+
+  def self.all
+    @@all ||= scrape_switch_games
+  end
+
 end
