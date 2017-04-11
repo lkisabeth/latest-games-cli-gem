347b5170bf1fce795ec849fe4a72f5ce9f87b078
Lucas Kisabeth <lukey.mac@outlook.com>
2017-04-11 00:56:56 +0000

    added #intialize and #self.all methods to NewGames::PCGame class

--- lib/new_games/pcgame.rb
+++ lib/new_games/pcgame.rb
@@ -1,4 +1,13 @@
 class NewGames::PCGame
   attr_accessor :name, :url, :release_date, :metascore, :publisher, :also_on
 
+  def initialize(name = nil, url = nil)
+    @name = name
+    @url = url
+  end
+
+  def self.all
+    @@all ||= scrape_pc_games
+  end
+
 end
