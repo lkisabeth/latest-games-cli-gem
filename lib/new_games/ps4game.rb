c93649b673e7c5574dc196ad828c9e596bc4730b
Lucas Kisabeth <lukey.mac@outlook.com>
2017-04-11 00:54:04 +0000

    added #initialize and #self.all methods to NewGames::PS4Game class

--- lib/new_games/ps4game.rb
+++ lib/new_games/ps4game.rb
@@ -1,4 +1,13 @@
 class NewGames::PS4Game
   attr_accessor :name, :url, :release_date, :metascore, :publisher, :also_on
 
+  def initialize(name = nil, url = nil)
+    @name = name
+    @url = url
+  end
+
+  def self.all
+    @@all ||= scrape_ps4_games
+  end
+
 end
