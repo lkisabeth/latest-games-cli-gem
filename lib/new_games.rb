34219c0cfaf8e52bf422dea06593ee054dea2044
Lucas Kisabeth <lukey.mac@outlook.com>
2017-04-11 00:25:09 +0000

    Refactor: added [platform]game.rb file for each platform and included require_relative's in new_games.rb

--- lib/new_games.rb
+++ lib/new_games.rb
@@ -3,7 +3,11 @@ require 'nokogiri'
 
 require_relative "new_games/version"
 require_relative "new_games/cli"
-require_relative "new_games/game"
+require_relative "new_games/ps4game"
+require_relative "new_games/xbonegame"
+require_relative "new_games/switchgame"
+require_relative "new_games/pcgame"
+require_relative "new_games/iosgame"
 
 module NewGames
 end
