01c7c038bb30f6817b994c0ad5ce9c007abaeb07
Lucas Kisabeth <lukey.mac@outlook.com>
2017-04-14 20:50:07 +0000

    added :summary attr_accessor to all game classes

--- lib/latest_games/pcgame.rb
+++ lib/latest_games/pcgame.rb
@@ -1,5 +1,5 @@
 class LatestGames::PCGame
-  attr_accessor :name, :url, :release_date, :metascore, :publisher, :also_on
+  attr_accessor :name, :url, :summary, :release_date, :metascore, :publisher, :also_on
 
   def initialize(name = nil, url = nil)
     @name = name
--- lib/latest_games/ps4game.rb
+++ lib/latest_games/ps4game.rb
@@ -1,5 +1,5 @@
 class LatestGames::PS4Game
-  attr_accessor :name, :url, :release_date, :metascore, :publisher, :also_on
+  attr_accessor :name, :url, :summary, :release_date, :metascore, :publisher, :also_on
 
   def initialize(name = nil, url = nil)
     @name = name
