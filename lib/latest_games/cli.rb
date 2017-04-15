3b1232731e946af364e7676f1da40eaa78c2d2e5
Lucas Kisabeth <lukey.mac@outlook.com>
2017-04-15 00:27:20 +0000

    added additional functionality to #start method

--- lib/latest_games/cli.rb
+++ lib/latest_games/cli.rb
@@ -15,46 +15,60 @@ class LatestGames::CLI
     puts "3) Switch"
     puts "4) PC"
     puts "5) iOS"
+    puts ""
+    puts "(To quit, enter 'exit' at any time..)"
   end
 
   def start
     self.list_platforms
     input = nil
-    while input != "exit"
+    until input == "exit"
       puts ""
-      puts "Enter the corresponding number to select a platform:"
+      puts "Select a platform:"
       input = gets.strip
-      if input.to_i == 0 && input != "exit"
-        puts ""
-        puts "Please enter the number, not the platform name!"
-        puts "Restarting..."
-        sleep(2)
-        start
-      elsif input.to_i == 1
+      if input.to_i == 1 || input.downcase == "ps4"
         platform = LatestGames::PS4Game.new
         list_games(platform)
+      elsif input.to_i == 2 || input.downcase == "xbox one"
+        platform = LatestGames::XboneGame.new
+        list_games(platform)
+      elsif input.to_i == 3 || input.downcase == "switch"
+        platform = LatestGames::SwitchGame.new
+        list_games(platform)
+      elsif input.to_i == 4 || input.downcase == "pc"
+        platform = LatestGames::PCGame.new
+        list_games(platform)
+      elsif input.to_i == 5 || input.downcase == "ios"
+        platform = LatestGames::IOSGame.new
+        list_games(platform)
+      elsif input.to_i == 0 && input != "exit"
+        puts ""
+        puts "That didn't quite work.. check your spelling and try again!"
+        puts "Reprinting list..."
+        sleep(2)
+        start
       end
     end
     puts ""
     puts "Hope you found something you like!"
     puts "Have a great day! :D"
-  end
+end
 
   def list_games(platform)
     puts ""
     if platform.is_a? LatestGames::PS4Game
       puts "*************** Latest PS4 Games ***************"
-    elsif platform.is_a? LatestGames::IOSGame.new
+    elsif platform.is_a? LatestGames::IOSGame
       puts "*************** Latest iOS Games ***************"
-    elsif platform.is_a? LatestGames::XboneGame.new
+    elsif platform.is_a? LatestGames::XboneGame
       puts "************* Latest Xbox One Games ************"
-    elsif platform.is_a? LatestGames::PCGame.new
+    elsif platform.is_a? LatestGames::PCGame
       puts "*************** Latest PC Games ****************"
-    elsif platform.is_a? LatestGames::SwitchGame.new
+    elsif platform.is_a? LatestGames::SwitchGame
       puts "************* Latest Switch Games **************"
     end
     puts ""
-    #@platform.all.each.with_index(1) {|game, i|
+    #platform.all.each.with_index(1) {|game, i|
       #puts "#{i}) #{game.name}  #{game.metascore}"
     #}
     puts ""
